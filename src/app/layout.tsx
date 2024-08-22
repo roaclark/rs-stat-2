5ad22a6576be2fa15514b80d99c6f1fb88afe9fe
Alex Rosalia <alex@rosalia.me>
2024-08-22 02:04:35 -0700

    Fix font issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import _ from "lodash";
 import { Divider } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import Link from "next/link";
 import skills from "./skills";
+
+import theme from "./theme";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -27,28 +31,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <nav className="p-12 pt-8 pb-0 max-w-screen-lg mx-auto">
-          <div className="flex justify-center gap-x-4 p-2">
-            <Link href="/">Overview</Link>•<Link href="/quests">Quests</Link>•
-            <Link href="/achievements">Achievements</Link>•
-            <Link href="/combat">Combat</Link>
-          </div>
-          <Divider />
-          <div className="flex flex-wrap justify-center gap-x-2 p-2 pt-4">
-            {_.keys(skills).map((skill, i) => {
-              return (
-                <>
-                  {i > 0 ? "•" : undefined}
-                  <Link href={`/skills/${skills[skill].name}`}>
-                    {skills[skill].name}
-                  </Link>
-                </>
-              );
-            })}
-          </div>
-        </nav>
-        <div className="p-8">{children}</div>
+      <body className={`font-sans ${geistSans.variable} ${geistMono.variable}`}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <nav className="p-12 pt-8 pb-0 max-w-screen-lg mx-auto">
+              <div className="flex justify-center gap-x-4 p-2">
+                <Link href="/">Overview</Link>•
+                <Link href="/quests">Quests</Link>•
+                <Link href="/achievements">Achievements</Link>•
+                <Link href="/combat">Combat</Link>
+              </div>
+              <Divider />
+              <div className="flex flex-wrap justify-center gap-x-2 p-2 pt-4">
+                {_.keys(skills).map((skill, i) => {
+                  return (
+                    <>
+                      {i > 0 ? "•" : undefined}
+                      <Link href={`/skills/${skills[skill].name}`}>
+                        {skills[skill].name}
+                      </Link>
+                    </>
+                  );
+                })}
+              </div>
+            </nav>
+            <div className="p-8">{children}</div>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
---
+++ src/app/theme.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import localFont from "next/font/local";
+import { createTheme } from "@mui/material/styles";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [geistSans.style.fontFamily, geistMono.style.fontFamily].join(
+      ","
+    ),
+  },
+});
+
+export default theme;
