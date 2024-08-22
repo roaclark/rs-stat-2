f77a307ffd3ec0bc46c33c8b6cb52c3bf10614f8
Alex Rosalia <alex@rosalia.me>
2024-08-22 01:32:00 -0700

    Add skill target table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Chip from "@mui/material/Chip";
+import _ from "lodash";
+
 import skills from "./skills";
 
 const statsGridOrder = [
@@ -41,6 +50,74 @@ export default function Overview() {
             </div>
           );
         })}
+      </div>
+      <div className="pt-8">
+        <TableContainer>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>Skill</TableCell>
+                <TableCell align="right">Level</TableCell>
+                <TableCell align="right">Quests</TableCell>
+                <TableCell align="right">Easy diary</TableCell>
+                <TableCell align="right">Medium diary</TableCell>
+                <TableCell align="right">Hard diary</TableCell>
+                <TableCell align="right">Elite diary</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {_.map(skills, (skill, key) => (
+                <TableRow
+                  key={key}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell>{skill.name}</TableCell>
+                  <TableCell align="right">{30}</TableCell>
+                  <TableCell align="right">
+                    <Chip
+                      label={10}
+                      color="success"
+                      size="small"
+                      variant="outlined"
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <Chip
+                      label={40}
+                      color="success"
+                      size="small"
+                      variant="outlined"
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <Chip
+                      label={50}
+                      color="success"
+                      size="small"
+                      variant="outlined"
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <Chip
+                      label={60}
+                      color="success"
+                      size="small"
+                      variant="outlined"
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <Chip
+                      label={70}
+                      color="error"
+                      size="small"
+                      variant="outlined"
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
