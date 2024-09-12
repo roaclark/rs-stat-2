b4002b1cb1a09eb6697743d9743dfc86d4e6e8ad
Alex Rosalia <alex@rosalia.me>
2024-09-11 19:51:30 -0700

    Fix hydration error

--- src/context/PlayerLevelContext.tsx
+++ src/context/PlayerLevelContext.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import _ from "lodash";
-import { createContext, useActionState } from "react";
+import { createContext, useEffect, useState, useTransition } from "react";
 import skills from "../data/skills";
 import fetchStats from "../actions/fetchStats";
 
 export const PlayerLevelContext = createContext({
   stats: _.mapValues(skills, () => ({ level: 0, experience: 0 })),
   update: () => {},
-  isPending: false,
+  isPending: true,
 });
 
 const getPlayerLevels = () => {
@@ -18,11 +18,23 @@ const getPlayerLevels = () => {
 };
 
 const PlayerLevelProvider = ({ children }: { children: React.ReactNode }) => {
-  const [stats, updateStats, isPending] = useActionState(async () => {
-    const data = await fetchStats();
-    localStorage.setItem("stats", JSON.stringify(data));
-    return data;
-  }, getPlayerLevels());
+  const [stats, setStats] = useState({});
+  const [isPending, startTransition] = useTransition();
+
+  function updateStats() {
+    startTransition(async () => {
+      const data = await fetchStats();
+      localStorage.setItem("stats", JSON.stringify(data));
+      setStats(data);
+    });
+  }
+
+  useEffect(() => {
+    startTransition(async () => {
+      const data = getPlayerLevels();
+      setStats(data);
+    });
+  }, []);
 
   return (
     <PlayerLevelContext.Provider
